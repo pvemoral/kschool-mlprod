4d6388e69e698f239ce8d556c5b6b592a4f03fc7
Pedro Esteban <kschool.pedro.esteban@gmail.com>
2021-01-23 08:28:50 +0000

    Function to train and evaluatel

--- trainer/task.py
+++ trainer/task.py
@@ -16,10 +16,12 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import optimizers
 from tensorflow.keras import losses
 from tensorflow.keras import metrics
+from tensorflow.keras import utils
 
-
+LOGGER = logging.getLogger()
 
 def _download_data():
+    logging.info("Download data")
     train, test = datasets.mnist.load_data()
     
     x_train, y_train = train
@@ -29,22 +31,29 @@ def _download_data():
 
 
 def _preprocess_data(x_train, y_train):
+    logging.info("Preporcess data")
     x_train = x_train / 255.0
-
     y_train = utils.to_categorical(y_train)
 
+    return x_train, y_train
 
 def _build_model():
-    m = models.Sequential()
+    logging.info("Create model")
+    model = models.Sequential()
 
-    m.add(layers.Input((28,28), name='my_input_layer'))
-    m.add(layers.Flatten())
-    m.add(layers.Dense(128, activation=activations.relu))
-    m.add(layers.Dense(64, activation=activations.relu))
-    m.add(layers.Dense(32, activation=activations.relu))
-    m.add(layers.Dense(10, activation=activations.softmax))
+    model.add(layers.Input((28,28), name='my_input_layer'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation=activations.relu))
+    model.add(layers.Dense(64, activation=activations.relu))
+    model.add(layers.Dense(32, activation=activations.relu))
+    model.add(layers.Dense(10, activation=activations.softmax))
 
     return model
+
+def _train_model(model, x_train, y_train, batch_size, epochs, validation_split=0.15):
+
+    return model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+    
 
 def train_and_evaluate(batch_size, epoch, job_dir, output_path):
 
@@ -52,24 +61,35 @@ def train_and_evaluate(batch_size, epoch, job_dir, output_path):
     x_train, y_train, x_test, y_test = _download_data()
 
     # Processes the data
-    x_train, y_train = _preprocess_data(x_train, y_train)
-    x_test, y_test = _preprocess_data(x_test, y_test)
+    x_train_p, y_train_p = _preprocess_data(x_train, y_train)
+    x_test_p, y_test_p = _preprocess_data(x_test, y_test)
     
     # Build the model
 
     model = _build_model()
+    model.compile(loss=losses.categorical_crossentropy, 
+                  optimizer=optimizers.Adam(), 
+                  metrics=[metrics.categorical_crossentropy])
     # Train the model
+    model.fit(x_train_p, y_train_p, batch_size=batch_size, epochs=epoch)
 
     # Evalutate de model
-    pass
+    loss_value, accuracy = model.evaluate(x_test_p, y_test_p)
+    logging.info("loss_value:{loss_value}, accuracy:{accuracy}".format(loss_value = loss_value, accuracy = accuracy))
+
+    #return loss_value, accuracy
 
 def main():
     """Entry point for your module."""
     # PARAMETROS QUE NECESITAMOS
-
+    # los hiperparametros que queramos añadir al programa se tiene que pasar por linea de comandos. 
+    # Cualquier parámetro que queramos añadir para que el optimizador de hiperparámetros los pueda modificar
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', type=int, help='Batch size for the training')
     parser.add_argument('--epochs', type=int, help='Number of epochs for the training')
+    # Estos parámetros son opciones de para AI platform
+    # '--job-dir' obligatorio
+    # '--model-output-path' path donde escribiremos el binario del modelo después de entrenarlo y que utilizaremos en producción.
     parser.add_argument('--job-dir', default=None, required=False, help='Option for AI Platform')
     parser.add_argument('--model-output-path', help='Path to write the SaveModel format')
 
