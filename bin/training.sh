09c58f637a330d5aeb2254c2860b16b0b45081e1
Pedro Esteban <kschool.pedro.esteban@gmail.com>
2021-01-23 09:05:17 +0000

    add training script

---
+++ bin/training.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+EPOCHS=30
+BATCH_SIZE=1024
+
+gcloud ai-platform jobs submit training mnist_`date +"%s"` \
+    --python-version 3.7 \
+    --runtime-version 2.3 \
+    --scale-tier BASIC \
+    --package-path ./trainer \
+    --module-name trainer.task \
+    --region europe-west1 \
+    --job-dir gs://kschool-dl-20210123/tmp \
+    -- \
+    --epochs $EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --model-output-path gs://kschool-dl-20210123/models \
