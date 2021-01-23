a1389f68654a48ce3eed3403f10554e97e0d3a14
Pedro Esteban <kschool.pedro.esteban@gmail.com>
2021-01-23 09:21:15 +0000

    add bucket variable in the job script

--- bin/training.sh
+++ bin/training.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 EPOCHS=30
 BATCH_SIZE=1024
+BUCKET=kschool-dl-20210123
 
 gcloud ai-platform jobs submit training mnist_`date +"%s"` \
     --python-version 3.7 \
@@ -9,8 +10,8 @@ gcloud ai-platform jobs submit training mnist_`date +"%s"` \
     --package-path ./trainer \
     --module-name trainer.task \
     --region europe-west1 \
-    --job-dir gs://kschool-dl-20210123/tmp \
+    --job-dir gs://$BUCKET/tmp \
     -- \
     --epochs $EPOCHS \
     --batch-size $BATCH_SIZE \
-    --model-output-path gs://kschool-dl-20210123/models \
+    --model-output-path gs://$BUCKET/models \
